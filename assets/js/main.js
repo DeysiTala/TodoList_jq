d67473916d13d9441389a686607236aa29202a4d
Deysi <sude.90@gmail.com>
2017-09-25 10:58:09 -0500

    detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,41 @@
-function addTodoItem() {
-  var todoItem = $("#new-to-do-item").val();
-  $("#to-do-list").append("<li><input type='checkbox'" + 
-                         " name='to-do-item-done'" + 
-                         " class='to-do-item-done'"+ 
-                         " value='" + todoItem + "' /> " + 
-                         todoItem +
-                         " <button type='button' class='to-do-item-delete close' aria-label='Close'>"+
-                         "<span aria-hidden='true'>&times;</span></button></li>");
-  /*<button type="button" class="close" aria-label="Close">
+function addToDo() {
+    var to_do = $("#new-to-do-item").val();
+    $("#to-do-list").append("<li><input type='checkbox'" +
+        " name='to-do-item-done'" +
+        " class='to-do-item-done'" +
+        " value='" + to_do + "' /> " +
+        to_do +
+        " <button type='button' class='to-do-item-closer close text-center btn btn-danger' aria-label='Close'>" +
+        "<span aria-hidden='true'>&times;</span></button></li>");
+    /*<button type="button" class="close" aria-label="Close">
   <span aria-hidden='true'>&times;</span>
 </button>*/
- $("#new-to-do-item").val("");
+    $("#new-to-do-item").val("");
 }
 
-function deleteTodoItem(e, item) {
-  e.preventDefault();
-  $(item).parent().fadeOut('slow', function() { 
-    $(item).parent().remove();
-  });
+function closeX(e, item) {
+    e.preventDefault();
+    $(item).parent().fadeOut('slow', function() {
+        $(item).parent().remove();
+    });
 }
 
-                           
-function completeTodoItem() {  
-  $(this).parent().toggleClass("strike");
+function completeToDo() {
+    $(this).parent().toggleClass("strike");
 }
-
 
 $(function() {
- 
-   $("#add-to-do-item").on('click', function(e){
-     e.preventDefault();
-     addTodoItem()
-   });
-  
-  $("#to-do-list").on('click', '.to-do-item-delete', function(e){
-    var item = this; 
-    deleteTodoItem(e, item)
-  })
-  
-  $(document).on('click', ".to-do-item-done", completeTodoItem)
+
+    $("#add-to-do-item").on('click', function(e) {
+        e.preventDefault();
+        addToDo()
+    });
+
+    $("#to-do-list").on('click', '.to-do-item-closer', function(e) {
+        var item = this;
+        closeX(e, item)
+    })
+
+    $(document).on('click', ".to-do-item-done", completeToDo)
 
 });
